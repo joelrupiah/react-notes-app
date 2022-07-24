708b885a7bc84cf064abe3d7f625582747428183
Joel Rupiah <rupiahjeremiah@gmail.com>
2022-07-24 15:20:53 +0000

    update start

--- src/components/pages/NotePage.jsx
+++ src/components/pages/NotePage.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import {ReactComponent as ArrowLeft} from '../../assets/arrow-left.svg'
 
 const NotePage = () => {
+  const navigate = useNavigate()
   const { id } = useParams()
   let noteId = id
 
@@ -17,9 +19,29 @@ const NotePage = () => {
     setNote(data)
   }
 
+  let updateNote = async () => {
+    fetch(`/notes/${noteId}/update`, {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(note)
+    })
+  }
+
+  let handleSubmit = () => {
+    updateNote()
+    navigate('/')
+  }
+
     return (
-        <div>
-          <h1>{note?.body}</h1>
+        <div className="note">
+          <div className="note-header" onClick={() => handleSubmit()}>
+            <h3>
+              <ArrowLeft />
+            </h3>
+          </div>
+          <textarea onChange={(e) => {setNote({...note, 'body':e.target.value})}} defaultValue={note?.body}></textarea>
         </div>
     )
 }
