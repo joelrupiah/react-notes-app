6029e79ee59779ce9ffad4fc03b4aa318ed51973
Joel Rupiah <rupiahjeremiah@gmail.com>
2022-07-25 09:29:39 +0000

    complete project

--- src/components/pages/NotePage.jsx
+++ src/components/pages/NotePage.jsx
@@ -1,49 +1,86 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import {ReactComponent as ArrowLeft} from '../../assets/arrow-left.svg'
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { ReactComponent as ArrowLeft } from "../../assets/arrow-left.svg";
 
 const NotePage = () => {
-  const navigate = useNavigate()
-  const { id } = useParams()
-  let noteId = id
+  const navigate = useNavigate();
+  const { id } = useParams();
+  let noteId = id;
 
-  let [note, setNote] = useState(null)
+  let [note, setNote] = useState(null);
 
   useEffect(() => {
-    getNote()
-  }, [noteId])
+    getNote();
+  }, [noteId]);
 
   let getNote = async () => {
-    let response = await fetch(`/notes/${noteId}`)
-    let data = await response.json()
-    setNote(data)
-  }
+    if(noteId === 'new') return
+    let response = await fetch(`/notes/${noteId}`);
+    let data = await response.json();
+    setNote(data);
+  };
+
+  let createNote = async () => {
+    fetch(`/notes/create`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    });
+  };
 
   let updateNote = async () => {
     fetch(`/notes/${noteId}/update`, {
       method: "PUT",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(note)
-    })
-  }
+      body: JSON.stringify(note),
+    });
+  };
+
+  let deleteNote = async () => {
+    fetch(`/notes/${noteId}/delete`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    navigate("/");
+  };
 
   let handleSubmit = () => {
-    updateNote()
-    navigate('/')
-  }
+    if(noteId !== 'new' && note.body === ''){
+      deleteNote()
+    }else if(noteId !== 'new' ){
+      updateNote()
+    }else if(noteId === 'new' && note.body !== null){
+      createNote()
+    }
+    navigate("/");
+  };
 
-    return (
-        <div className="note">
-          <div className="note-header" onClick={() => handleSubmit()}>
-            <h3>
-              <ArrowLeft />
-            </h3>
-          </div>
-          <textarea onChange={(e) => {setNote({...note, 'body':e.target.value})}} defaultValue={note?.body}></textarea>
-        </div>
-    )
-}
+  return (
+    <div className="note">
+      <div className="note-header">
+        <h3>
+          <ArrowLeft onClick={() => handleSubmit()} />
+        </h3>
+        {noteId !== "new" ? (
+          <button onClick={deleteNote}>Delete</button>
+        ) : (
+          <button onClick={handleSubmit}>Done</button>
+        )}
+      </div>
+      <textarea
+        onChange={(e) => {
+          setNote({ ...note, body: e.target.value });
+        }}
+        value={note?.body}
+      ></textarea>
+    </div>
+  );
+};
 
-export default NotePage
+export default NotePage;
